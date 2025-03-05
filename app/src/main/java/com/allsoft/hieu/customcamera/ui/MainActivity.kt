e4d37e2828bd3d0df39cd67da34e53727b7b8b26
Hieutm0507 <hieutm.22bi13161@usth.edu.vn>
2025-03-05 16:48:28 +0700

    CHANGED using RegisterForActivityResult instead of requestPermissions

--- app/src/main/java/com/allsoft/hieu/customcamera/ui/MainActivity.kt
+++ app/src/main/java/com/allsoft/hieu/customcamera/ui/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.allsoft.hieu.customcamera.ui
 
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
@@ -17,10 +21,28 @@ import androidx.core.view.WindowInsetsCompat
 import com.allsoft.hieu.customcamera.R
 import com.allsoft.hieu.customcamera.databinding.ActivityMainBinding
 import com.allsoft.hieu.customcamera.utils.Constants
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var imageCapture: ImageCapture
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+        if (permissions.all { it.value }) {
+            startCamera()
+        }
+        else {
+            Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show()
+            finish()        // Đóng ứng dụng khi quyền ko được cấp
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,27 +61,61 @@ class MainActivity : AppCompatActivity() {
             startCamera()
         }
         else {
-            ActivityCompat.requestPermissions(this, Constants.REQUIRED_PERMISSIONS, Constants.REQUEST_CODE_PERMISSIONS)
+            launcher.launch(Constants.REQUIRED_PERMISSIONS)
+        }
+
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        binding.ibChangeCamera.setOnClickListener {
+            takePhoto()
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
 
-    // TODO: Handle Permission
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray,
-        deviceId: Int
-    ) {
-        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionGranted()) {
-                startCamera()
-            }
-            else {
-                Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show()
-                finish()
+    private fun getOutputDirectory() : File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let { mFile ->
+            File(mFile, resources.getString(R.string.app_name)).apply {
+                mkdirs()
             }
         }
+
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    @SuppressLint("WeekBasedYear")
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?:  return
+        val photoFile = File(outputDirectory,
+            SimpleDateFormat(Constants.FILE_NAME_FORMAT, Locale.getDefault())
+                .format(System.currentTimeMillis()) + ".jpg")
+
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+
+        // takePicture(outputFileOption, executor, object : ImageCapture.OnImageSavedCallback)
+        // Ảnh lưu trong thư mục Phone/Android/media/
+        imageCapture.takePicture(
+            outputOption,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo Saved"
+
+                    Toast.makeText(this@MainActivity, "$msg $savedUri", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(Constants.TAG, "onError: ${exception.message}", exception)
+                }
+            }
+        )
     }
 
 
